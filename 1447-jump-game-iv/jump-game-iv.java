34964d74f3be29fb2c6dfc085c7a1bf27530d089
Dhanaraj Patil <dhanraajnpatil@gmail.com>
2026-05-18 00:09:28 -0700

    Time: 45 ms (96.62%) | Memory: 75.4 MB (92.64%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -1,65 +1,47 @@
 class Solution {
-    public int minJumps(int[] arr) 
-    {
+    public int minJumps(int[] arr) {
         int n = arr.length;
+        if(n==1) return 0;
 
-        HashMap<Integer, List<Integer>> mp = new HashMap<>();
-
-        for(int i = 0; i < n; i++)
-        {
-            mp.putIfAbsent(arr[i], new ArrayList<>());
-            mp.get(arr[i]).add(i);
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            if(!map.containsKey(arr[i])) map.put(arr[i], new ArrayList<>());
+            map.get(arr[i]).add(i);
         }
 
         Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[n];
+        boolean[] vis = new boolean[n];
+        q.add(n-1);
+        vis[n-1]=true;
+        int count=0, x;
 
-        q.offer(0);
-        visited[0] = true;
+        while(!q.isEmpty()) {
+            int sz = q.size();
+            count++;
+            for(int k=0; k<sz; k++) {
+                int ind = q.poll();
+                
+                x=ind-1;
+                if(x==0) return count;
+                if(x>=0 && x<n && !vis[x]) {vis[x]=true; q.add(x);}
 
-        int steps = 0;
+                x=ind+1;
+                if(x==0) return count;
+                if(x>=0 && x<n && !vis[x]) {vis[x]=true; q.add(x);}
 
-        while(!q.isEmpty())
-        {
-            int size = q.size();
-
-            while(size-- > 0)
-            {
-                int currIdx = q.poll();
-
-                if(currIdx == n - 1) return steps;
-
-                //================================================================
-                //EXPLORE ALL POSSIBLE OPTIONS
-
-                if(currIdx + 1 < n && !visited[currIdx + 1]) //OPTION-1 (Move Forward)
-                {
-                    visited[currIdx + 1] = true;
-                    q.offer(currIdx + 1);
-                }
-
-                if(currIdx - 1 >= 0 && !visited[currIdx - 1]) //OPTION-2 (Move Backward)
-                {
-                    visited[currIdx - 1] = true;
-                    q.offer(currIdx - 1);
-                }
-
-                for(int newIdx : mp.get(arr[currIdx])) //OPTION-3 (Move to same valued idx)
-                {                                      //newIdx could be before currIdx or after currIdx
-                    if(!visited[newIdx])
-                    {
-                        visited[newIdx] = true;
-                        q.offer(newIdx);
+                List<Integer> list = map.get(arr[ind]);
+                if(list!=null) {
+                for(int i : list){
+                    if(!vis[i]) {
+                        if(i==0) return count;
+                        vis[i]=true;
+                        q.add(i);
                     }
                 }
-
-                //===================================================================
-                mp.get(arr[currIdx]).clear(); //EXPLAINED BELOW :)
+                }
+                map.remove(arr[ind]);
             }
-
-            steps++;
         }
-
         return -1;
     }
 }
