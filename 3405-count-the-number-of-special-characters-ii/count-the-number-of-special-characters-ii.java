5108f949288687bbb355b319152b6282e3c0affc
Dhanaraj Patil <dhanraajnpatil@gmail.com>
2026-05-26 22:22:44 -0700

    Time: 7 ms (100.00%) | Memory: 48.1 MB (64.88%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -1,49 +1,24 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public int numberOfSpecialChars(String word) {
-        int[] lastLower = new int[26];
-        int[] firstUpper = new int[26];
-
-        Arrays.fill(lastLower, -1);
-        Arrays.fill(firstUpper, -1);
-
-        Set<Integer> invalid = new HashSet<>();
-
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') {
-                int idx = ch - 'a';
-
-                lastLower[idx] = i;
-
-                if (firstUpper[idx] != -1) {
-                    invalid.add(idx);
-                }
-
-            } else {
-                int idx = ch - 'A';
-
-                if (firstUpper[idx] == -1) {
-                    firstUpper[idx] = i;
-                }
-            }
-        }
-
-        int specialCount = 0;
-
-        for (int i = 0; i < 26; i++) {
-            if (lastLower[i] != -1 &&
-                firstUpper[i] != -1 &&
-                !invalid.contains(i)) {
-
-                specialCount++;
-            }
-        }
-
-        return specialCount;
-    }
+		int n = word.length();
+		char[] chars = word.toCharArray();
+		int[] lastLower = new int[26];
+		int[] firstUpper = new int[26];
+		Arrays.fill(lastLower, -1);
+		Arrays.fill(firstUpper, -1);
+		for (int i = 0; i < n; i++) {
+			if (chars[i] >= 'a') {
+				lastLower[chars[i] - 'a'] = i;
+			} else if (firstUpper[chars[i] - 'A'] == -1) {
+				firstUpper[chars[i] - 'A'] = i;
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < 26; i++) {
+			if (lastLower[i] != -1 && lastLower[i] < firstUpper[i]) {
+				ans++;
+			}
+		}
+		return ans;
+	}
 }
