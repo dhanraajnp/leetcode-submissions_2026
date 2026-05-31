66c08b44a205130b17c2009fbbce894c25e217c1
Dhanaraj Patil <dhanraajnpatil@gmail.com>
2026-05-30 19:52:26 -0700

    Time: 4 ms (100.00%) | Memory: 105.7 MB (95.84%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -1,18 +1,23 @@
-class Solution 
-{
-    public boolean asteroidsDestroyed(int mass, int[] asteroids) 
-    {
-        Arrays.sort(asteroids);
-        int n=asteroids.length;
-        for(int i=0;i<n;i++)
-        {
-            if(asteroids[n-1]<=mass)
-            break;
-            if(mass<asteroids[i])
-            return false;
-            mass+=asteroids[i];
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        int min = 100000;
+        int max = 0;
+        for(int x : asteroids) {
+            max = Math.max(x, max);
+            min = Math.min(x, min);
+        }
+
+        int[] freq = new int[max + 1];
+        long val = mass;
+        for(int x : asteroids) {
+            if(x > val) freq[x]++;
+            else val += x;
+        }
+        if(val >= max) return true;
+        for(int i = min; i <= max; i++) {
+            if(val < i) return false;
+            if(freq[i] != 0) val += i * freq[i];
         }
         return true;
-
     }
 }
