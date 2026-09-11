07f5edb915ee67231a29ae35b9f97744d67b786f
Dhanaraj Patil <dhanraajnpatil@gmail.com>
2026-09-10 22:39:30 -0700

    Time: 1 ms (100.00%) | Memory: 43.7 MB (96.13%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -1,21 +1,33 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        int[] f = new int[10];
-        int res = 0;
+        int[] freq = new int[10];
+        for(int digit:digits) freq[digit]++;
+        int zero=0, even=0, all=0;
 
-        for (int d : digits) f[d]++;
-
-        for (int i = 1; i < 10; i++) 
-            for (int j = 0; j < 10; j++) 
-                for (int k = 0; k < 9; k += 2) {
-                    f[i]--; f[j]--; f[k]--;
-
-                    if (f[i] >= 0 && f[j] >= 0 && f[k] >= 0) res++;
-
-                    f[i]++; f[j]++; f[k]++;
-                }           
+        for(int i=0; i<10; i++)if (freq[i]>0){
+            if (i==0) zero++;
+            if (i%2==0) even++;
+            all++;
+        }
         
 
-        return res;
+        int count = even*(all-1)*(all-2);
+        if (zero==1) count-=(even-1)*(all-2);
+
+        for(int i=0; i<10; i++){
+            if (freq[i]>=2){
+                if (i==0) count+=all-1;
+                else if (i%2==1) count+=even;
+                else{
+                    count+=3*(even-1)-zero;
+                    count+=2*(all-even);
+                }           
+            }
+        }
+
+        for(int i=2; i<10; i+=2)if (freq[i]>=3)count++;
+            
+        
+        return count;
     }
 }
